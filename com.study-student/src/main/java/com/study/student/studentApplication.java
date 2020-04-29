9c0aac240199a0071b4ee897143b340655b02ac9
huangda <1823242250@qq.com>
2020-04-29 10:10:38 +0800

    搭建springboot环境

---
+++ com.study-student/src/main/java/com/study/student/studentApplication.java
@@ -0,0 +1,14 @@
+package com.study.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class studentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(studentApplication.class, args);
+	}
+
+}
